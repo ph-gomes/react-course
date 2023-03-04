863befce3a5f60b0b45d481902c6d791fab54adc
ph-gomes <rphdgomes@gmail.com>
2023-03-03 21:48:45 -0300

    init project 2.0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import Todo from './components/Todo';
-
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text="Deserunt occaecat." />
-      <Todo text="Laboris commodo officia." />
-      <Todo text="Velit minim qui et occaecat." />
+      <h1>Hello World</h1>
     </div>
   );
 }
--- src/components/Backdrop.js
+++
@@ -1,6 +0,0 @@
-const Backdrop = (props) => {
-  const { onClick } = props;
-  return <div className="backdrop" onClick={onClick} />;
-};
-
-export default Backdrop;
