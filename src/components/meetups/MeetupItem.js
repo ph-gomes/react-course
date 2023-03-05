cd5cf193dc38cd4d8cb65045b72490d299bd1d43
ph-gomes <rphdgomes@gmail.com>
2023-03-04 22:11:36 -0300

    toggle favorites

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,8 +1,21 @@
+import { useContext } from 'react';
+import FavoritesContext from '../../store/favorites-context';
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
 const MeetupItem = (props) => {
-  const { title, image, address, description } = props;
+  const { title, image, address, description, id } = props;
+
+  const favoritesCtx = useContext(FavoritesContext);
+
+  const itemIsFavorite = favoritesCtx.itemIsFavorite(id);
+
+  const toggleFavoritesStatusHandler = () => {
+    if (itemIsFavorite) {
+      return favoritesCtx.removeFavorite(id);
+    }
+    return favoritesCtx.addFavorite({ title, image, address, description, id });
+  };
 
   return (
     <li className={classes.item}>
@@ -16,7 +29,9 @@ const MeetupItem = (props) => {
           <p>{description}</p>
         </div>
         <div className={classes.actions}>
-          <button>To Favorites</button>
+          <button onClick={toggleFavoritesStatusHandler}>
+            {itemIsFavorite ? 'Remove from Favorites' : 'To Favorites'}
+          </button>
         </div>
       </Card>
     </li>
