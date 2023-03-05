04285f9e1a296ece87442fd31e287c986ff3f7a5
ph-gomes <rphdgomes@gmail.com>
2023-03-04 22:17:12 -0300

    list favorites meetups

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,21 @@
+import { useContext } from 'react';
+import MeetupList from '../components/meetups/MeetupList';
+import FavoritesContext from '../store/favorites-context';
+
 const FavoritesPage = () => {
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You got no favorites yet</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+
   return (
     <div>
       <h1>My Favorites</h1>
+      {content}
     </div>
   );
 };
